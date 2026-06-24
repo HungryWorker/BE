1a766d4f73921ab2f46613ebe8f66414f5a0aa0c
이혜정 <lhj1227lhj@naver.com>
2026-06-24 16:43:14 +0900

    init: project setting

---
+++ src/test/java/com/_2/hungryworker/HungryworkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com._2.hungryworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HungryworkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
